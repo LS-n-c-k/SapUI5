529872b19eaec26e00c2b8e52eac3a5b13779598
Chandan Kalita <chandan.kalita190@gmail.com>
2016-07-21 17:31:48 +0530

    Add files via upload

---
+++ ObjectPageSection/Component.js
@@ -0,0 +1,25 @@
+sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("sap.uxap.sample.ObjectPageSection.Component", {
+
+		metadata: {
+			rootView: "sap.uxap.sample.ObjectPageSection.ObjectPageSection",
+			dependencies: {
+				libs: [
+					"sap.m"
+				]
+			},
+			config: {
+				sample: {
+					stretch: true,
+					files: [
+						"ObjectPageSection.view.xml",
+						"ObjectPageSection.controller.js"
+					]
+				}
+			}
+		}
+	});
+	return Component;
+}, true);
